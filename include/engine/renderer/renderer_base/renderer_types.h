ab4d3766431426f4d07837229cd81c371985d919
chocolate-pie24 <chocolatepie099@gmail.com>
2025-12-21 13:48:37 +0900

    feat: レンダラー全体から使用されるデータ構造定義を追加

---
+++ include/engine/renderer/renderer_base/renderer_types.h
@@ -0,0 +1,55 @@
+/** @ingroup renderer_base
+ *
+ * @file renderer_types.h
+ * @author chocolate-pie24
+ * @brief レンダラーレイヤー全体で使用されるデータ型を提供する
+ *
+ * @version 0.1
+ * @date 2025-12.19
+ *
+ * @copyright Copyright (c) 2025 chocolate-pie24
+ *
+ * @par License
+ * MIT License. See LICENSE file in the project root for full license text.
+ *
+ */
+#ifndef GLCE_ENGINE_RENDERER_BASE_RENDERER_TYPES_H
+#define GLCE_ENGINE_RENDERER_BASE_RENDERER_TYPES_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief レンダラーレイヤー実行結果コード定義
+ *
+ */
+typedef enum {
+    RENDERER_SUCCESS = 0,       /**< 処理成功 */
+    RENDERER_INVALID_ARGUMENT,  /**< 無効な引数 */
+    RENDERER_RUNTIME_ERROR,     /**< 実行時エラー */
+    RENDERER_NO_MEMORY,         /**< メモリ確保失敗 */
+    RENDERER_UNDEFINED_ERROR,   /**< 不明なエラー */
+} renderer_result_t;
+
+/**
+ * @brief OpenGL固有の型を上位層で使用しないで済むよう、GPUに転送する頂点情報のデータ種別(書き換え頻度)リスト
+ *
+ */
+typedef enum {
+    BUFFER_USAGE_STATIC,    /**< データ種別: 頻繁に書き換わることのないデータ */
+    BUFFER_USAGE_DYNAMIC,   /**< データ種別: 頻繁に書き換わるデータ */
+} buffer_usage_t;
+
+/**
+ * @brief OpenGL固有の型を上位層で使用しないで済むよう、OpenGL固有型の上位型を定義
+ *
+ */
+typedef enum {
+    RENDERER_TYPE_FLOAT,    /**< データ型: GLfloat */
+} renderer_type_t;
+
+#ifdef __cplusplus
+}
+#endif
+#endif
