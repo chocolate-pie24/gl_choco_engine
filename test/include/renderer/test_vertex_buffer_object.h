ae1c9a87b0b0e7b9803ebce7c78b24389f5d9143
chocolate-pie24 <chocolatepie099@gmail.com>
2025-12-21 14:01:08 +0900

    feat: レンダラーレイヤーのテスト関数実行を追加

---
+++ test/include/renderer/test_vertex_buffer_object.h
@@ -0,0 +1,15 @@
+#ifndef GLCE_TEST_TEST_TEST_VERTEX_BUFFER_OBJECT_H
+#define GLCE_TEST_TEST_TEST_VERTEX_BUFFER_OBJECT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef TEST_BUILD
+void test_vertex_buffer_object(void);
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+#endif
