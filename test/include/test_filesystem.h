aed74e2336581dc5abf2109b7ca6a57c39185861
chocolate-pie24 <chocolatepie099@gmail.com>
2025-12-29 09:48:08 +0900

    feat: 新規モジュールファイルシステムを追加(ファイルオープン,クローズ,バイト単位での読み取りといった最もローレベルな機能を提供する)

---
+++ test/include/test_filesystem.h
@@ -0,0 +1,15 @@
+#ifndef GLCE_TEST_TEST_FILESYSTEM_H
+#define GLCE_TEST_TEST_FILESYSTEM_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef TEST_BUILD
+void test_filesystem(void);
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+#endif
