50a8616796f174c9fae674edef1b568cfc459675
chocolate-pie24 <chocolatepie099@gmail.com>
2026-04-01 16:42:55 +0900

    test: choco_mathのテストディレクトリを変更

---
+++ test/include/engine/base/choco_math/test_choco_math.h
@@ -0,0 +1,29 @@
+/**
+ * @file test_choco_math.h
+ * @author chocolate-pie24
+ * @brief choco_mathモジュール用テストAPI定義
+ *
+ * @version 0.1
+ * @date 2026-04-01
+ *
+ * @copyright Copyright (c) 2026 chocolate-pie24
+ *
+ * @par License
+ * MIT License. See LICENSE file in the project root for full license text.
+ *
+ */
+#ifndef GLCE_TEST_ENGINE_BASE_CHOCO_MATH_TEST_CHOCO_MATH_H
+#define GLCE_TEST_ENGINE_BASE_CHOCO_MATH_TEST_CHOCO_MATH_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef TEST_BUILD
+void test_choco_math(void);
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+#endif
