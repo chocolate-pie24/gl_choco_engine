d96d5f6e0740fd265756a0fc4b1377993e7c9167
chocolate-pie24 <chocolatepie099@gmail.com>
2026-05-15 08:55:43 +0900

    docs: vertex.hドキュメント追加

--- include/engine/core/geometry_premitive/vertex.h
+++ include/engine/core/geometry_premitive/vertex.h
@@ -1,3 +1,18 @@
+/** @ingroup geometry_premitive
+ *
+ * @file vertex.h
+ * @author chocolate-pie24
+ * @brief 形状データを構成する基本的な幾何情報構造体定義
+ *
+ * @version 0.1
+ * @date 2026-05-14
+ *
+ * @copyright Copyright (c) 2026 chocolate-pie24
+ *
+ * @par License
+ * MIT License. See LICENSE file in the project root for full license text.
+ *
+ */
 #ifndef GLCE_ENGINE_CORE_GEOMETRY_PREMITIVE_VERTEX_H
 #define GLCE_ENGINE_CORE_GEOMETRY_PREMITIVE_VERTEX_H
 
@@ -13,7 +28,7 @@ extern "C" {
  */
 typedef struct ui_vertex {
     vec2f_t position;   /**< 頂点座標 */
-    vec2f_t tex_coord; /**< テクスチャUV座標 */
+    vec2f_t tex_coord;  /**< テクスチャUV座標 */
 } ui_vertex_t;
 
 #ifdef __cplusplus
