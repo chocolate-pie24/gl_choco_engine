fcdcc2e20f8779c12314cfc7d9e2c80271e6a19b
chocolate-pie24 <chocolatepie099@gmail.com>
2026-07-09 16:38:08 +0900

    fix: range_free_list_status_get不要処理削除

--- src/engine/core/memory/range_free_list.c
+++ src/engine/core/memory/range_free_list.c
@@ -314,23 +314,16 @@ void range_free_list_status_get(const range_free_list_t* range_free_list_, range
     out_status_->unused_node_count = range_free_list_->unused_node_count;
 
     node = range_free_list_->free_block_list_head;
-    if(NULL == node) {
-        out_status_->free_block_count = 0;
-        out_status_->total_free_size = 0;
-        out_status_->max_free_block_size = 0;
-    } else {
-        tmp_max = node->block_size;
-        while(NULL != node) {
-            tmp_max = (node->block_size > tmp_max) ? node->block_size : tmp_max;
-            total_free_size += node->block_size;
-            free_block_count++;
+    while(NULL != node) {
+        tmp_max = (node->block_size > tmp_max) ? node->block_size : tmp_max;
+        total_free_size += node->block_size;
+        free_block_count++;
 
-            node = node->next;
-        }
-        out_status_->max_free_block_size = tmp_max;
-        out_status_->free_block_count = free_block_count;
-        out_status_->total_free_size = total_free_size;
+        node = node->next;
     }
+    out_status_->max_free_block_size = tmp_max;
+    out_status_->free_block_count = free_block_count;
+    out_status_->total_free_size = total_free_size;
 }
 
 // 要求サイズを満たす最初の空き領域ノードをfree_block_list_headから探索する。
