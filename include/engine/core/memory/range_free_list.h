af42203f40e7d605f6ac7c2a27af4f096bbc5092
chocolate-pie24 <chocolatepie099@gmail.com>
2026-07-10 12:37:18 +0900

    docs(core/memory): RangeFreeListの使用方法について設計メモを追加

--- include/engine/core/memory/range_free_list.h
+++ include/engine/core/memory/range_free_list.h
@@ -1,3 +1,20 @@
+// NOTE: 本モジュールは、将来的にrenderer/renderer/core/allocatorsに移動し、当面はOpenGLバックエンド用のVBO / EBO管理専用のアロケータとする。
+// 以下の理由により、CPU側メモリリソース確保には使用しない
+//
+// 理由1:
+// freeするためにallocated_sizeが必要、allocated_sizeはアライメントの都合上、required_sizeとは異なる場合がある。
+// CPU側では通常、sizeof(type) x countでメモリ使用量を管理することが多く、個別に確保サイズ変数を記憶しておくことは少ない。
+// VBOではoffsetを管理する必要があるため、この問題は影響が少ないが、CPU側では使いにくく、APIの誤用が増える可能性がある。
+//
+// 理由2:
+// Range Free Listでは、空きノード数に上限を設けている。このため、メモリチャンクが多く発生すると、空きノードが足りなくなり、freeに失敗する可能性がある
+// CPU側リソースでは、アセットのロード中など、頻繁なアロケーションと頻繁なfreeが発生する場合がある。
+// このため、起動時に正確な空きノードの数を見積もるのが難しい。よって、CPU側メモリリソース確保には、Range Free Listではなく、通常のFree Listを使用することにする。
+// GPU側リソース確保についても、同様の問題は発生するが、CPU側のように通常のFree ListはメモリプールがGPU上にある都合上使用できない。そのためRange Free Listを使用するが、
+// 以下の対策を行うことで、空きノード不足に対応する
+// - vbo_poolを導入し、dynamic_arrayで管理する
+// - vbo_poolはvbo_pageの配列で、vbo_pageはvboとrange_free_listを保持する
+// - 空きノードが足りなくなった場合、vbo_poolの配列要素を増やす
 #ifndef GLCE_ENGINE_CORE_MEMORY_RANGE_FREE_LIST_H
 #define GLCE_ENGINE_CORE_MEMORY_RANGE_FREE_LIST_H
 
