5249cfcb47be1d0ea7164e76cb191263d4df3506
chocolate-pie24 <chocolatepie099@gmail.com>
2026-09-18 09:23:20 +0900

    clean: 古いテストスタイルのテストコードを削除

--- glce/application/entry.c
+++ glce/application/entry.c
@@ -15,40 +15,6 @@
 
 #include "engine/base/choco_message.h"
 
-#ifdef TEST_BUILD   // TODO: test用のmainを用意して別に移す
-
-// test: application/core
-#include "application/core/test_application_err_utils.h"
-
-// test: application/command_interpreter
-
-// test: engine/base
-#include "engine/base/choco_math/test_choco_math.h"
-
-// test: engine/core
-#include "engine/core/memory/test_choco_memory.h"
-#include "engine/core/buffer_utils/test_buffer_utils.h"
-#include "engine/core/geometry_primitive/test_aabb_3d.h"
-#include "engine/core/geometry_primitive/test_geometry_primitive_err_utils.h"
-
-// test: engine/containers
-
-// test: engine/io_utils
-
-// test: engine/resource
-#include "engine/resource/loaders/test_bmp_loader.h"
-#include "engine/resource/loaders/test_stl_loader.h"
-#include "engine/resource/geometry/test_lit_mesh_geometry.h"
-#include "engine/resource/geometry/test_line_mesh_geometry.h"
-#include "engine/resource/geometry/test_point_mesh_geometry.h"
-#include "engine/resource/geometry/test_ui_mesh_geometry.h"
-
-// test: engine/systems/camera_system
-#include "engine/systems/camera_system/camera_core/test_camera_err_utils.h"
-#include "engine/systems/camera_system/camera_core/test_camera_memory.h"
-
-#endif
-
 /**
  * @brief ゲームアプリケーションメイン
  *
@@ -68,38 +34,6 @@ int main(int argc_, char** argv_) {
 #endif
 #ifdef TEST_BUILD
     INFO_MESSAGE("Build mode: TEST.");
-    for(uint8_t i = 0; i != 200; ++i) {
-        message_output(100, NULL);
-
-        // application/core
-        // test_application_err_utils();
-
-        // application/command_interpreter
-
-        // engine/base
-        test_choco_math();
-
-        // engine/core
-        test_choco_memory();
-        test_buffer_utils();
-        test_aabb_3d();
-        test_geometry_primitive_err_utils();
-
-        // engine/containers
-
-        // engine/io_utils
-
-        // engine/resource
-        // test_resource_err_utils();
-        // test_bmp_loader();
-        // test_stl_loader();
-        // test_lit_mesh_geometry();
-        // test_line_mesh_geometry();
-        // test_point_mesh_geometry();
-        // test_ui_mesh_geometry();
-
-        // engine/camera
-    }
 #endif
     application_result_t app_run_result = APPLICATION_INVALID_ARGUMENT;
     application_result_t app_create_result = APPLICATION_INVALID_ARGUMENT;
